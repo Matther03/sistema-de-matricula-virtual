6028ccc54c0826d3c20119afebec794bcbd31504
Maycol Soto <sotomaycol03@gmail.com>
2022-01-28 08:20:06 -0500

    Procedure_login

--- server/api_rest_sistema_matricula/src/main/java/database/ProceduresDB.java
+++ server/api_rest_sistema_matricula/src/main/java/database/ProceduresDB.java
@@ -1,9 +1,15 @@
 package database;
 
 public class ProceduresDB {
-    public static final String GET_ESTUDENTS = "CALL sp_get_dogs(?);";//reparar falta
+    public static final String GET_ESTUDENTS = "CALL sp_get_dogs(?);";
+//reparar falta
     public static final String FIND_STUDENT = "CALL sp_find_dog(?);"; 
     public static final String ADD_DOG = "CALL sp_add_dog(?, ?, ?, ?, ?, ?);";
     public static final String UPDATE_DOG = "CALL sp_update_dog(?, ?, ?, ?, ?, ?, ?);";
-    public static final String CARRY_DOG = "CALL sp_carry_dog(?);";    
+    public static final String CARRY_DOG = "CALL sp_carry_dog(?);";  
+    
+    
+    //matricula Procedures
+    public static final String GET_PASSWORD = "CALL sp_get_password(?);";
+    
 }
