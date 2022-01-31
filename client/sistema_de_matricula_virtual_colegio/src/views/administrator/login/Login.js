17567cebf6c5e5c81bd7260f1d6ab4e804356af9
Ralf <ralfcarrascostein@gmail.com>
2022-01-31 12:29:19 -0500

    Pequeño progreso en el login del admin

---
+++ client/sistema_de_matricula_virtual_colegio/src/views/administrator/login/Login.js
@@ -0,0 +1,147 @@
+//#region libraries
+import { 
+    useState,
+    useEffect
+} from 'react';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
+import {
+    InputLabel,
+    Button,
+    InputAdornment,
+    IconButton
+} from '@mui/material';
+//#endregion
+//#region Styles
+import { 
+    ContainerSectionLogin,
+    ContentSectionLogin,
+    AdminImgContainer,
+    ContentFormSectionLogin
+} from './styles';
+//#endregion
+//#region Images
+import adminImg from '../../../img/administrator/login/admin-icon.png';
+//#endregion
+//#region Icons
+import { Icon } from '@iconify/react';
+//#endregion
+//#region Components
+import SymbolHeader from "../../../components/campus/components/symbolHeader/SymbolHeader";
+import CustomTextField from '../../../components/general/customTextField/CustomTextField';
+//#endregion
+//#region Services
+import { loginAdmin, isLoggedAdmin } from '../../../services/auth';
+//#endregion
+const regex = {
+    user: /^[0-9]{8}$/,
+    password: /^.{8,16}$/
+}
+
+const Login = () => {
+    //#region States
+    const [form, setForm] = useState({
+        user: "",
+        password: ""
+    });
+    const [showPassword, setShowPassword] = useState(false);
+    const [errors, setErrors] = useState({
+        user: false,
+        password: false
+    });
+    //#endregion
+    //#region Effects
+    useEffect(() => {
+        validateField("user");
+    }, [form.user]);
+    useEffect(() => {
+        validateField("password");
+    }, [form.password]);
+    //#endregion
+    //#region Extra hooks
+    const navigate = useNavigate();
+    //#endregion
+    //#region Functions
+    const handleChangeTextField = (e, field) => {
+        const { value } = e.target;
+        setForm(prev => ({
+            ...prev,
+            [field]: value
+        }));
+    }
+    const toggleShowPassword = () => {
+        setShowPassword(prev => (!prev));
+    }
+    const validateField = (field) => {
+        setErrors(prev => ({
+            ...prev,
+            [field]: !regex[field].test(form[field])
+        }));
+    }
+    const handleLogin = (e) => {
+        e.preventDefault();
+        loginAdmin({ 
+            user: form.user, 
+            password: form.password 
+        });
+        isLoggedAdmin() && navigate("/admin/home");
+    }
+    //#endregion
+    return (
+        <>
+            {isLoggedAdmin() && <Navigate to="../home" replace={true}/>}
+            <SymbolHeader className="adminHeader"/>
+            <ContainerSectionLogin>
+                <ContentSectionLogin>
+                    <AdminImgContainer>
+                        <img src={adminImg} alt="Icono Admin"/>
+                    </AdminImgContainer>
+                    <h2 className="custom-title-2">INICIAR SESIÓN</h2>
+                    <ContentFormSectionLogin
+                        onSubmit={handleLogin}>
+                        <section className="fields">
+                            <div>
+                                <InputLabel className="admin-input-label">Usuario</InputLabel>
+                                <CustomTextField 
+                                        className="admin-input"
+                                        variant="outlined"
+                                        value={form.user}
+                                        onChange={(e) => handleChangeTextField(e, "user")}
+                                        error={errors.user}
+                                        />
+                            </div>
+                            <div>
+                                <InputLabel className="admin-input-label">Contraseña</InputLabel>
+                                <CustomTextField 
+                                    className="admin-input"
+                                    type={showPassword ? "text" : "password"}
+                                    value={form.password}
+                                    onChange={(e) => handleChangeTextField(e, "password")}
+                                    error={errors.password}
+                                    InputProps={{ 
+                                        endAdornment: (
+                                            <InputAdornment >
+                                                <IconButton
+                                                onClick={toggleShowPassword}>
+                                                    <Icon icon={showPassword 
+                                                        ? "ic:sharp-visibility-off"
+                                                        : "ic:round-visibility"} />
+                                                </IconButton>
+                                            </InputAdornment>
+                                        )
+                                    }}/>
+                            </div>
+                        </section>
+                        <footer>
+                        <Button 
+                            className="login-btn"
+                            type="submit"
+                            disabled={errors.user || errors.password}>Ingresar</Button>
+                        </footer>
+                    </ContentFormSectionLogin>
+                </ContentSectionLogin>
+            </ContainerSectionLogin>
+        </>
+    );
+}
+
+export default Login;
---
+++ client/sistema_de_matricula_virtual_colegio/src/views/administrator/login/styles.js
@@ -0,0 +1,67 @@
+//#region Libraries
+import styled from "styled-components";
+//#endregion
+
+export const ContainerSectionLogin = styled.section`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: blue;
+    min-height: 100vh;
+    margin-top: 81px;
+    background-color: var(--secondary-blue);
+    
+`;
+export const ContentSectionLogin = styled.article`
+    display:flex;
+    flex-direction: column;
+    position: relative;
+    z-index: 0;
+    background-color: var(--third-color);
+    padding: 40px;
+    border-radius: 30px;
+    justify-content: center;
+    align-items: center;
+    & h2 {
+        margin: 65px 0 30px 0;
+    }
+`;
+export const AdminImgContainer = styled.div`
+    position: absolute;
+    top: -18%;
+    left: 31%;
+    background-color: var(--fourth-color);
+    border-radius: 50%;
+    padding: 25px;
+    box-shadow: 5px 5px 10px 0px #00000040;
+    & img {
+        width: 100px;
+    }
+`
+export const ContentFormSectionLogin = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 50px;
+    width: 100%;
+    & .fields {
+        display: flex;
+        flex-direction: column;
+        gap: 15px;
+        width: 100%;
+    }
+    & .login-btn {
+        background-color: var(--seventh-color);
+        width: 150px;
+        border-radius: 4px;
+    }
+    & .login-btn:hover {
+        background-color: var(--seventh-color-dark);
+    }
+    & .login-btn:active {
+        box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%);
+    }
+    & .admin-input-label {
+        font-weight: 700;
+    }
+`
