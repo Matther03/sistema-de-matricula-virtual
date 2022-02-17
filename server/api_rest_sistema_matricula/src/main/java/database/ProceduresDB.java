c3c8f874729de60e39b9954dafd1c25fead58858
Maycol Soto <sotomaycol03@gmail.com>
2022-02-17 11:52:02 -0500

    Procedures sp_get_detail_classroom && sp_get_grade_to_enrollment (bug solucionado)

--- server/api_rest_sistema_matricula/src/main/java/database/ProceduresDB.java
+++ server/api_rest_sistema_matricula/src/main/java/database/ProceduresDB.java
@@ -7,7 +7,7 @@ public class ProceduresDB {
     public static final String GET_DETAIL_STUDENT = "CALL sp_get_detail_student(?);";
     public static final String VERIFY_PAY = "CALL sp_verify_payment_student(?);";
     public static final String VERYFY_GRADE = "CALL sp_verify_grade_student(?);";
-        public static final String GET_GRADE_TO_ENROLLMENT = "CALL sp_get_grade_to_enrollment(?);"; 
+    public static final String GET_GRADE_TO_ENROLLMENT = "CALL sp_get_grade_to_enrollment(?);"; 
     public static final String VERIFY_ENROLL = "CALL sp_verify_enrollment(?);";
     public static final String DO_ENROLLMENT = "CALL sp_do_enrollment(?,?,?);";
     public static final String GET_DETAIL_ENROLLMENT = "CALL sp_get_detail_enrollment(?);";
